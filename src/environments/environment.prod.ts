bfe78126684e71b04293afff03b1143b61eb3b38
Eduardo Sanz García <esanzgar@gmail.com>
2019-03-11 19:37:20 +0000

    WIP chore: initial setup firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
+//  var config = {
+//    apiKey: "AIzaSyDNBC67WKxs4miuKi-xz4QirICxKY1BR7w",
+//    authDomain: "pubmed-6c453.firebaseapp.com",
+//    databaseURL: "https://pubmed-6c453.firebaseio.com",
+//    projectId: "pubmed-6c453",
+//    storageBucket: "",
+//    messagingSenderId: "40696183324"
+//  };
+//
 export const environment = {
   epmcUrl: "https://www.ebi.ac.uk/europepmc/webservices/rest/search",
   ncbiUrl: "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/",
