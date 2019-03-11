0ad0a6171246b1fa6f73d43d05a8230cc87e4bce
Eduardo Sanz García <esanzgar@gmail.com>
2019-03-11 19:45:22 +0000

    chore: setup database firebase

--- src/reader/reader.module.ts
+++ src/reader/reader.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireDatabaseModule } from "@angular/fire/database";
+
+import { firebaseAppConfig } from "../environments/environment";
 
 import { ReaderRoutingModule } from "./reader-routing.module";
 import { MaterialModule } from "../material/material.module";
@@ -8,6 +12,11 @@ import { ReadingListComponent } from "./containers/reading-list/reading-list.com
 
 @NgModule({
   declarations: [ReadingListComponent],
-  imports: [CommonModule, ReaderRoutingModule, MaterialModule]
+  imports: [
+    CommonModule,
+    ReaderRoutingModule,
+    AngularFireModule.initializeApp(firebaseAppConfig),
+    MaterialModule
+  ]
 })
 export class ReaderModule {}
